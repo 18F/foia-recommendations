447ea1a81e5ec4152ae3c7f27a172dea3f5f5132
Aaron D Borden <adborden@a14n.net>
2017-05-16 14:41:33 -0700

    Pull in uswds prototype scaffolding (#30)
    * Pull in uswds prototype scaffolding
    
    * Use bundle exec around Jekyll
    
    This simplifies the install, if you've already got bundler, you don't need to
    worry about installing the right version of jekyll.

---
+++ prototypes/form-wizard/src/index.js
@@ -0,0 +1,7 @@
+/* src/index.js
+ *
+ * Add  your custom javascript here. This file is only loaded on the home page
+ * unless you add a `javascript` property to your page's front matter.
+ **/
+
+console.log('hello world');
