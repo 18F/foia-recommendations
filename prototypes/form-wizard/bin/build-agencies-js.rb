3a6adbcbc064650734acc41f0e5351cc0c401642
Peter Karman <peter.karman@gsa.gov>
2017-06-15 14:20:13 -0500

    Create make target for building agencies.js
    * Include department and agency abbreviations in the search-able name.
    * Pull build code into its own script with its own make target.

---
+++ prototypes/form-wizard/bin/build-agencies-js.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'yaml'
+require 'fileutils'
+require 'pp'
+
+class JsBuilder
+  DEFAULT_YAML_DIR = __dir__ + '/../../../../2015-foia/contacts/data'
+  OUTPUT_DIR = __dir__ + '/../assets/js'
+  OUTPUT_FILE = OUTPUT_DIR + '/agencies.js'
+
+  def generate
+    agencies = load_yaml_files(ENV.fetch('FOIA_AGENCY_DIR', DEFAULT_YAML_DIR))
+    write_page(agencies)
+  end 
+
+  def load_yaml_files(dir)
+    agencies = {}
+    Dir.glob("#{dir}/*.yaml") do |yml|
+      agency = YAML.load_file(yml)
+      load_agency(agencies, agency)
+    end 
+    agencies
+  end 
+
+  def load_agency(agencies, agency)
+    departments = agency['departments']
+    if departments.count == 1
+      if agency['abbreviation']
+        name = sprintf("%s ( %s )", agency['name'], agency['abbreviation'])
+      else
+        name = agency['name']
+      end
+      agencies[name] = departments[0]
+    else
+      departments.each do |dept|
+        if dept['top_level'] == true
+          name = agency['name']
+        elsif dept['abbreviation'] && agency['abbreviation']
+          name = sprintf("%s ( %s ), %s ( %s )",\
+                   dept['name'], dept['abbreviation'], agency['name'], agency['abbreviation'])
+        elsif agency['abbreviation']
+          name = sprintf("%s, %s ( %s )", dept['name'], agency['name'], agency['abbreviation'])
+        else
+          name = sprintf("%s, %s", dept['name'], agency['name'])
+        end 
+        agencies[name] = dept
+      end 
+    end 
+  end 
+
+  def write_page(agencies)
+    FileUtils.mkdir_p(File.dirname(OUTPUT_FILE))
+    File.open(OUTPUT_FILE, 'w+') do |file|
+      file.write "var AGENCIES = #{agencies.to_json};"
+    end 
+  end
+end
+
+JsBuilder.new.generate
