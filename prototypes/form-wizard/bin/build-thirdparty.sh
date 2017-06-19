a611768a38c8d15708a5e25f8c502ef536f66868
Aaron D Borden <adborden@a14n.net>
2017-06-19 16:01:03 -0700

    Use absolute urls (#95)
    * Use absolute urls
    
    When sharing a url that includes the user/pass, Chrome blocks some assets in an
    attempt to hide the creds. By using absolute urls, we can use a link with
    user/pass to auth the user and all assets won't leak any information.
    
    * Set baseurl only for dist builds

--- prototypes/form-wizard/bin/build-thirdparty.sh
+++ prototypes/form-wizard/bin/build-thirdparty.sh
@@ -9,5 +9,3 @@ mkdir -p _site/assets/vendor
 cp -R node_modules/uswds/src/fonts node_modules/uswds/src/img _site/assets/
 cp node_modules/uswds/dist/js/uswds.min.js _site/assets/vendor/
 cp node_modules/jquery/dist/jquery.min.js _site/assets/vendor/
-
-bundle exec jekyll build
